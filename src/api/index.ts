1889efc63cbe23c5f6366833b7e84f2032d97b6c
Azael Guerrero <76462603+azael20@users.noreply.github.com>
2023-09-23 17:40:42 -0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,8 @@ import {memo} from "react";
 
 const useRestaurantsService = () => {
     const getRestaurantsFromApi = () => {
-        const url = `https://api.yelp.com/v3/businesses/search?location=ensenada&sort_by=best_match&limit=20`;
-        const apiKey = 'Kq1jhQIzioJ55T23Xx5TA_q_2vcepmoItljnsA3r6bpS_QUXiZszjgLJlg0z_aixwnM-SNJbC8kPOTVDo9T7kWR7JokUz8-Ow1M6TGlJB9ZOFOozU2r6pGVMvu0MZXYx';
+        const url = ``;
+        const apiKey = '';
 
         const options: RequestInit = {
             headers: {
